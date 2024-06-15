5ee06a666b485ca7372e2e75c77b47cef684b479
nazennash <nazennash120@gmail.com>
2024-06-16 01:34:02 +0300

    product_retrieve

--- products/views.py
+++ products/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 import random
 from rest_framework.pagination import PageNumberPagination
+from django.utils import timezone
+from datetime import timedelta
+from rest_framework.decorators import action
+from rest_framework import status
 
 
 class ProductPagination(PageNumberPagination):
@@ -17,6 +21,7 @@ class ProductPagination(PageNumberPagination):
 class MainCategoryView(viewsets.ModelViewSet):
     queryset = MainCategory.objects.all()
     serializer_class = MainCategorySerializer
+    
     
 class SubCategoryView(viewsets.ModelViewSet):
     queryset = SubCategory.objects.all()
@@ -30,3 +35,47 @@ class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductPagination
+
+    def get_queryset(self):
+        queryset = list(Product.objects.all())
+        random.shuffle(queryset)
+        return queryset
+    
+    def retrieve(self, request, pk=None):
+        queryset = Product.objects.all()
+        product = get_object_or_404(queryset, pk=pk)
+        serializer = ProductSerializer(product, many=False)
+
+        data = serializer.data
+        data["image"] = request.build_absolute_uri(data["image"])
+
+        return Response(data)
+
+    
+    @action(detail=False, methods=['get'])
+    def new_arrivals(self, request):
+        thirty_days_ago = timezone.now() - timedelta(days=3)
+        queryset = list(Product.objects.filter(created_at__gte=thirty_days_ago))
+        random.shuffle(queryset)
+        
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['get'], url_path='main_category/(?P<main_category_id>[^/.]+)')
+    def product_category(self, request, main_category_id=None):
+        queryset = list(Product.objects.filter(main_category_id=main_category_id))
+        random.shuffle(queryset)
+        
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
